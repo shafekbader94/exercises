9218f5b1ffef95d0c450a5ccd1f0b943b134653f
shafekbader94 <shafekgames@gmail.com>
2022-04-26 18:28:28 +0300

    OOP

---
+++ week-3/OOP Singleton & Dependency Injection/intro/singleton.js
@@ -0,0 +1,13 @@
+class DataManger{
+    constructor(){
+        this.data = this.getFromDB()
+    }
+
+    getFromDB(){
+        // some code that gets data from the DB
+    }
+
+    addData(data){
+        // code that saves the new piece of data to the DB
+    }
+}
---
+++ week-3/Object Oriented Programming Intro (OOP)/Exercise/main.js
@@ -0,0 +1,64 @@
+//write your code here to make the tests pass
+class Document {
+  constructor(EmployeeName) {
+    this.EmployeeName = EmployeeName;
+  }
+}
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  work(office) {
+    for (let i = 0; i < 10; i++) {
+      office.documents.push(new Document(this.name))
+    }
+  }
+}
+
+class Manager {
+  constructor(name) {
+    this.employees = [];
+    this.name = name;
+  }
+
+  hireEmployee(name) {
+    this.employees.push(new Employee(name));
+  }
+
+  askEmployeesToWork(office) {
+    this.employees.forEach((employee) => employee.work(office))
+  }
+}
+
+class Cleaner {
+  constructor(name) {
+    this.name = name;
+  }
+
+  clean(name) {
+    console.log("Clean");
+  }
+}
+
+class Office {
+  constructor() {
+    this.documents = [];
+    this.managers = [];
+    this.cleaners = [];
+  
+  }
+  hireCleaner(name) {
+    this.cleaners.push(new Cleaner(name))
+  }
+
+  hireManager(name) {
+    this.managers.push(new Manager(name))
+  }
+
+  startWorkDay() {
+    this.managers.forEach((manger) => manger.askEmployeesToWork(this))
+    this.cleaners.forEach((clean) => clean.clean())
+  }
+}
