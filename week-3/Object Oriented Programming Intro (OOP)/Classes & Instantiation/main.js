9a110e9f9f4ac1191fcbbce81885ba49c3c0b985
Shafek Bader <66371617+shafekbader94@users.noreply.github.com>
2022-04-27 19:32:21 +0300

    Delete week-3/Object Oriented Programming Intro (OOP) directory

--- week-3/Object Oriented Programming Intro (OOP)/Classes & Instantiation/main.js
+++
@@ -1,9 +0,0 @@
-class Animal {
-    constructor(name, numLegs) {
-        this.name = name
-        this.numLegs = numLegs
-    }
-}
-
-const cat = new Animal("Puss", 4)
-console.log(cat.name + " has " + cat.numLegs + " legs") //prints "Puss has 4 legs"
--- week-3/Object Oriented Programming Intro (OOP)/Spot Check 1/main.js
+++
@@ -1,9 +0,0 @@
-class Animal {
-    constructor(name, numLegs) {
-        this.name = name
-        this.numLegs = numLegs
-    }
-}
-
-const dog = new Animal("the dog", 4)
-console.log(dog.name) 
